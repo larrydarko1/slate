9aa1dca3484c7a0fef0cf32eb9957fefe605bd97
Larry Darko <l.mazzola96@tutanota.com>
2026-03-21 13:22:14 +0100

    chore: updated commitlint rules to allow more characters for the header and body
    security: update dependencies to fix vulnerabilities

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 200],
+        'body-max-line-length': [2, 'always', 200],
+        'type-enum': [
+            2,
+            'always',
+            ['build', 'bump', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+        ]
+    }
+};
